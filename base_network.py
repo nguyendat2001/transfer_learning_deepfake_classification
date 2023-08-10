dfb2e7569e4a53b69a284cdd0254fbc719adffdf
ntdat <datb1910206@student.ctu.edu.vn>
2023-08-10 09:45:23 +0700

    update

--- base_network.py
+++ base_network.py
@@ -8,103 +8,85 @@ import cv2
 import os
 import tensorflow 
 import tensorflow as tf
-from tensorflow.keras.layers import Dense, Activation,Dropout,Conv2D, MaxPooling2D,BatchNormalization
+from tensorflow.keras.layers import Dense, Activation,Dropout,Conv2D, MaxPooling2D,BatchNormalization, Flatten
 from tensorflow.keras.optimizers import Adamax
+from keras.models import Model
 
 
-def get_base(base_name, exp_dict, n_classes):
+def get_base(base_name, img_size, n_classes):
     
     if base_name == "xception":
-        base = tf.keras.applications.Xception(include_top=False, weights="imagenet", input_shape=(exp_dict['img_size'],exp_dict['img_size'],3),classes=n_classes,
-        classifier_activation="softmax")
+        base = tf.keras.applications.Xception(include_top=False, weights="imagenet", input_shape=(img_size,img_size,3) )
        
     if base_name == "vgg16":
-        base = tf.keras.applications.VGG16(include_top=False, weights="imagenet", input_shape=(exp_dict['img_size'],exp_dict['img_size'],3),classes=n_classes,
-        classifier_activation="softmax")
+        base = tf.keras.applications.VGG16(include_top=False, weights="imagenet", input_shape=(img_size,img_size,3) )
         
     if base_name == "vgg19":
-        base = tf.keras.applications.VGG19(include_top=False, weights="imagenet", input_shape=(exp_dict['img_size'],exp_dict['img_size'],3),classes=n_classes,
-        classifier_activation="softmax")
+        base = tf.keras.applications.VGG19(include_top=False, weights="imagenet", input_shape=(img_size,img_size,3) )
         
     if base_name == "resnet50":
-        base = tf.keras.applications.ResNet50(include_top=False, weights="imagenet", input_shape=(exp_dict['img_size'],exp_dict['img_size'],3),classes=n_classes,
-        classifier_activation="softmax")
+        base = tf.keras.applications.ResNet50(include_top=False, weights="imagenet", input_shape=(img_size,img_size,3) )
        
     if base_name == "resnet50v2":
-        base = tf.keras.applications.ResNet50V2(include_top=False, weights="imagenet", input_shape=(exp_dict['img_size'],exp_dict['img_size'],3),classes=n_classes,
-        classifier_activation="softmax")
+        base = tf.keras.applications.ResNet50V2(include_top=False, weights="imagenet", input_shape=(img_size,img_size,3) )
         
     if base_name == "resnet101":
-        base = tf.keras.applications.ResNet101(include_top=False, weights="imagenet", input_shape=(exp_dict['img_size'],exp_dict['img_size'],3),classes=n_classes,
-        classifier_activation="softmax")
+        base = tf.keras.applications.ResNet101(include_top=False, weights="imagenet", input_shape=(img_size,img_size,3) )
         
     if base_name == "resnet101v2":
-        base = tf.keras.applications.ResNet101V2(include_top=False, weights="imagenet", input_shape=(exp_dict['img_size'],exp_dict['img_size'],3),classes=n_classes,
-        classifier_activation="softmax")
+        base = tf.keras.applications.ResNet101V2(include_top=False, weights="imagenet", input_shape=(img_size,img_size,3) )
         
     if base_name == "resnet152":
-        base = tf.keras.applications.ResNet152(include_top=False, weights="imagenet", input_shape=(exp_dict['img_size'],exp_dict['img_size'],3),classes=n_classes,
-        classifier_activation="softmax")
+        base = tf.keras.applications.ResNet152(include_top=False, weights="imagenet", input_shape=(img_size,img_size,3) )
        
     if base_name == "resnet152v2":
-        base = tf.keras.applications.ResNet152V2(include_top=False, weights="imagenet", input_shape=(exp_dict['img_size'],exp_dict['img_size'],3),classes=n_classes,
-        classifier_activation="softmax")
+        base = tf.keras.applications.ResNet152V2(include_top=False, weights="imagenet", input_shape=(img_size,img_size,3) )
         
     if base_name == "inceptionv3":
-        base = tf.keras.applications.InceptionV3(include_top=False, weights="imagenet", input_shape=(exp_dict['img_size'],exp_dict['img_size'],3),classes=n_classes,
-        classifier_activation="softmax")
+        base = tf.keras.applications.InceptionV3(include_top=False, weights="imagenet", input_shape=(img_size,img_size,3) )
         
     if base_name == "inceptionrestnetv2":
-        base = tf.keras.applications.InceptionResNetV2(include_top=False, weights="imagenet", input_shape=(exp_dict['img_size'],exp_dict['img_size'],3),classes=n_classes,
-        classifier_activation="softmax")
+        base = tf.keras.applications.InceptionResNetV2(include_top=False, weights="imagenet", input_shape=(img_size,img_size,3) )
     
     if base_name == "mobilenet":
-        base = tf.keras.applications.MobileNet(include_top=False, weights="imagenet", input_shape=(exp_dict['img_size'],exp_dict['img_size'],3),classes=n_classes,
-        classifier_activation="softmax")
+        base = tf.keras.applications.MobileNet(include_top=False, weights="imagenet", input_shape=(img_size,img_size,3) )
        
     if base_name == "mobilenetv2":
-        base = tf.keras.applications.MobileNetV2(include_top=False, weights="imagenet", input_shape=(exp_dict['img_size'],exp_dict['img_size'],3),classes=n_classes,
-        classifier_activation="softmax")
+        base = tf.keras.applications.MobileNetV2(include_top=False, weights="imagenet", input_shape=(img_size,img_size,3) )
         
     if base_name == "densenet121":
-        base = tf.keras.applications.DenseNet121(include_top=False, weights="imagenet", input_shape=(exp_dict['img_size'],exp_dict['img_size'],3),classes=n_classes,
-        classifier_activation="softmax")
+        base = tf.keras.applications.DenseNet121(include_top=False, weights="imagenet", input_shape=(img_size,img_size,3) )
     
     if base_name == "densenet201":
-        base = tf.keras.applications.densenet201(include_top=False, weights="imagenet", input_shape=(exp_dict['img_size'],exp_dict['img_size'],3),classes=n_classes,
-        classifier_activation="softmax")
+        base = tf.keras.applications.densenet201(include_top=False, weights="imagenet", input_shape=(img_size,img_size,3) )
         
     if base_name == "efficientnetb0":
-        base = tf.keras.applications.EfficientNetB0(include_top=False, weights="imagenet", input_shape=(exp_dict['img_size'],exp_dict['img_size'],3),classes=n_classes,
-        classifier_activation="softmax")
+        base = tf.keras.applications.EfficientNetB0(include_top=False, weights="imagenet", input_shape=(img_size,img_size,3) )
         
     if base_name == "efficientnetb1":
-        base = tf.keras.applications.EfficientNetB1(include_top=False, weights="imagenet", input_shape=(exp_dict['img_size'],exp_dict['img_size'],3),classes=n_classes,
-        classifier_activation="softmax")
+        base = tf.keras.applications.EfficientNetB1(include_top=False, weights="imagenet", input_shape=(img_size,img_size,3) )
         
     if base_name == "efficientnetb2":
-        base = tf.keras.applications.EfficientNetB2(include_top=False, weights="imagenet", input_shape=(exp_dict['img_size'],exp_dict['img_size'],3),classes=n_classes,
-        classifier_activation="softmax")
+        base = tf.keras.applications.EfficientNetB2(include_top=False, weights="imagenet", input_shape=(img_size,img_size,3) )
         
     if base_name == "efficientnetb3":
-        base = tf.keras.applications.EfficientNetB3(include_top=False, weights="imagenet", input_shape=(exp_dict['img_size'],exp_dict['img_size'],3),classes=n_classes,
-        classifier_activation="softmax")
+        base = tf.keras.applications.EfficientNetB3(include_top=False, weights="imagenet", input_shape=(img_size,img_size,3) )
         
     if base_name == "efficientnetb4":
-        base = tf.keras.applications.EfficientNetB4(include_top=False, weights="imagenet", input_shape=(exp_dict['img_size'],exp_dict['img_size'],3),classes=n_classes,
-        classifier_activation="softmax")
+        base = tf.keras.applications.EfficientNetB4(include_top=False, weights="imagenet", input_shape=(img_size,img_size,3) )
         
     if base_name == "efficientnetb5":
-        base = tf.keras.applications.EfficientNetB5(include_top=False, weights="imagenet", input_shape=(exp_dict['img_size'],exp_dict['img_size'],3),classes=n_classes,
-        classifier_activation="softmax")
+        base = tf.keras.applications.EfficientNetB5(include_top=False, weights="imagenet", input_shape=(img_size,img_size,3) )
         
     if base_name == "efficientnetb6":
-        base = tf.keras.applications.EfficientNetB6(include_top=False, weights="imagenet", input_shape=(exp_dict['img_size'],exp_dict['img_size'],3),classes=n_classes,
-        classifier_activation="softmax")
+        base = tf.keras.applications.EfficientNetB6(include_top=False, weights="imagenet", input_shape=(img_size,img_size,3) )
         
     if base_name == "efficientnetb7":
-        base = tf.keras.applications.EfficientNetB7(include_top=False, weights="imagenet", input_shape=(exp_dict['img_size'],exp_dict['img_size'],3),classes=n_classes,
-        classifier_activation="softmax")
+        base = tf.keras.applications.EfficientNetB7(include_top=False, weights="imagenet", input_shape=(img_size,img_size,3) )
     
+    x = base.output
+    x = Flatten()(x)
     
-    return base
+    outs = Dense(n_classes, activation='softmax')(x)
+    model = Model(inputs=base.inputs, outputs= outs)
+    return model
--- train.py
+++ train.py
@@ -9,7 +9,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from keras.preprocessing.image import ImageDataGenerator
 from keras.optimizers import Adam
-from keras.utils import np_utils
 from imutils import paths
 from keras.models import Sequential
 from keras.layers import *
@@ -18,9 +17,6 @@ import numpy as np
 import pandas as pd
 import argparse
 import pickle
-from haven import haven_chk as hc
-from haven import haven_results as hr
-from haven import haven_utils as hu
 import keras
 import cv2
 import os
@@ -32,47 +28,100 @@ from tensorflow.keras.optimizers import Adamax
 from tensorflow.keras.models import Model
 from sklearn.preprocessing import OneHotEncoder
 import base_network
-# !pip install mediapipe
-# import mediapipe as mp
 
-n_class = 2
+from keras import backend as K
 
-def read_data(path):
-    def load_data(path):
-    file = open(path, 'rb')
+def recall_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
 
-    # dump information to that file
-    (pixels, labels) = pickle.load(file)
+def precision_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
 
-    # close the file
-    file.close()
-    le = LabelEncoder()
-    labels = le.fit_transform(labels)
-    labels = np_utils.to_categorical(labels, n_class)
-    pixels = np.array(pixels)
-    labels = np.array(labels)
-    print(pixels.shape)
-    print(labels.shape)
+def f1_m(y_true, y_pred):
+    precision = precision_m(y_true, y_pred)
+    recall = recall_m(y_true, y_pred)
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
 
-    return pixels, labels
+n_class = 5
+
+# def read_data(path):
+#     def load_data(path):
+#     file = open(path, 'rb')
+
+#     # dump information to that file
+#     (pixels, labels) = pickle.load(file)
+
+#     # close the file
+#     file.close()
+#     le = LabelEncoder()
+#     labels = le.fit_transform(labels)
+#     labels = np_utils.to_categorical(labels, n_class)
+#     pixels = np.array(pixels)
+#     labels = np.array(labels)
+#     print(pixels.shape)
+#     print(labels.shape)
+
+#     return pixels, labels
 
 
-def train_val(exp_dict , save_dir, data_dir):
+def train_val(args , save_dir):
+    
+    # train_datagen = ImageDataGenerator(
+    #     featurewise_center=True,
+    #     featurewise_std_normalization=True,
+    #     rotation_range=20,
+    #     width_shift_range=0.2,
+    #     height_shift_range=0.2,
+    #     horizontal_flip=True,
+    #     validation_split=0.2)
+    # valid_datagen = ImageDataGenerator(rescale=1./255)
+        
     figures_path = save_dir
     
-    X,y = read_data(data_dir)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    model = base_network.get_base(exp_dict['base'],exp_dict,2)
-    model.
+    model = base_network.get_base(args.base,args.im_size,5)
+    model.summary()
+    # model.
     
-    BS = exp_dict['batch_size']
+    BS = args.batch_size
     
-    aug = ImageDataGenerator(rotation_range=20, zoom_range=0.2,
+    aug = ImageDataGenerator(rescale=1./255, rotation_range=20, zoom_range=0.2,
         width_shift_range=0.2, height_shift_range=0.2, shear_range=0.15, brightness_range=[1,1.5],
         horizontal_flip=True, fill_mode="nearest")
     
+    aug_tmp = ImageDataGenerator(rescale=1./255)
+    
+    train_generator = aug.flow_from_directory(directory=args.train, 
+                                                    target_size=(args.im_size, args.im_size),
+                                                    # classes=['NORMAL','PNEUMONIA','TURBERCULOSIS'],
+                                                    # color_mode="grayscale",
+                                                    batch_size=64,
+                                                    class_mode="categorical",
+                                                    shuffle=True,seed=1234)
+    
+    val_generator = aug_tmp.flow_from_directory(directory=args.val,
+                                                    target_size=(args.im_size, args.im_size),
+                                                    # classes=['NORMAL','PNEUMONIA','TURBERCULOSIS'],
+                                                    # color_mode="grayscale",
+                                                    batch_size=64,
+                                                    class_mode="categorical",
+                                                    shuffle=True,seed=1234)
+    
+    test_generator = aug_tmp.flow_from_directory(directory=args.test,
+                                                    target_size=(args.im_size, args.im_size),
+                                                    # classes=['NORMAL','PNEUMONIA','TURBERCULOSIS'],
+                                                    # color_mode="grayscale",
+                                                    batch_size=64,
+                                                    class_mode="categorical",
+                                                    shuffle=True,seed=1234)
+    
     opt = Adam(lr=0.001)
-    model.compile(optimizer = opt, loss = "categorical_crossentropy", metrics = ["accuracy","AUC"])
+    model.compile(optimizer = opt, loss = "categorical_crossentropy", metrics = ["accuracy","AUC",f1_m,precision_m, recall_m])
     
     checkpoint_path = os.path.join(figures_path,"best_model.h5")
     checkpoint = keras.callbacks.ModelCheckpoint(checkpoint_path,
@@ -83,44 +132,56 @@ def train_val(exp_dict , save_dir, data_dir):
 #     earlystopping = keras.callbacks.EarlyStopping(monitor='val_accuracy', 
 #                                                   patience=15)
     
-    print("[INFO] training network for {} epochs...".format(EPOCHS))
-    history = model.fit(X_train,y_train,
-        validation_data=(X_test,y_test,BS), 
-        epochs=EPOCHS, batch_size = BS,
+    print("[INFO] training network for {} epochs...".format(args.epochs ))
+    history = model.fit(train_generator,
+        validation_data=val_generator, 
+        epochs=args.epochs,
         callbacks=[checkpoint])
 
     # eval
     model.save(os.path.join(figures_path,'model.h5'))
     
+    model.load_weights(checkpoint_path)
+    
     hist_df = pd.DataFrame(history.history)
     name_history = os.path.join(figures_path,'history.csv')
     with open(name_history, mode='w') as f:
         hist_df.to_csv(f)
-        
-#     loss_test,accuracy_test, auc_test = model.evaluate(X_test, y_test, batch_size=exp_dict['batch_size'])
-#     print('LOSS TEST: ', loss_test)
-#     print("ACCURACY TEST: ", accuracy_test)
-#     print('AUC TEST: ', auc_test)
+    
+    summaries = []
+    
+    loss_test,accuracy_test,auc_test ,f1_test,precision_test, recall_test = model.evaluate(test_generator)
+    print('LOSS TEST: ', loss_test)
+    print("ACCURACY TEST: ", accuracy_test)
+    print('AUC TEST: ', auc_test)
+    summaries.append([loss_test, accuracy_test, auc_test, f1_test, precision_test, recall_test])
 
-#     supervisor = 'LOSS TEST: '+ str(loss_test)+"\nACCURACY TEST: "+ str(accuracy_test)+'\nauc test: '+ str(auc_test)
+    pd.DataFrame(summaries).to_csv(os.path.join(figures_path, 'result.csv')) 
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-sb', '--savedir_base', default='CovidSeg/save')
-    parser.add_argument('-d', '--datadir', default='CovidSeg/dataset')
+    parser.add_argument('-tr', '--train', default='CovidSeg/dataset')
+    parser.add_argument('-te', '--test', default='CovidSeg/dataset')
+    parser.add_argument('-va', '--val', default='CovidSeg/dataset')
     parser.add_argument("-b", "--base", default='') # efficientnet-b0 
-    parser.add_argument("-bs", "--batch_size", type=int, default=2) # batch_size
-    parser.add_argument("-i", "--im_size", type=int, default=512) # image size for input
-    parser.add_argument("-e", "--epochs", type=int, default=512)
+    parser.add_argument("-bs", "--batch_size", type=int, default=64) # batch_size
+    parser.add_argument("-i", "--im_size", type=int, default=256) # image size for input
+    parser.add_argument("-e", "--epochs", type=int, default=100)
     parser.add_argument('-o', '--opt', default='adam') # optimizer adam or SGD 
 #     parser.add_argument('-ag', '--augmentation', type=bool, default=True) # optimizer adam or SGD 
     args = parser.parse_args()
-    exp_dict = []
-    exp_dict['base'] = args.base
-    exp_dict['img_size'] = args.im_size
-    exp_dict['batch_size'] = args.batch_size
-    exp_dict['optimizer'] = args.opt
-    exp_dict['epochs'] = args.epochs
+    # exp_dict = []
+    # exp_dict['base'] = args.base
+    # exp_dict['img_size'] = args.im_size
+    # exp_dict['batch_size'] = args.batch_size
+    # exp_dict['optimizer'] = args.opt
+    # exp_dict['epochs'] = args.epochs
+    # exp_dict['train'] = args.train
+    # exp_dict['test'] = args.test
+    # exp_dict['val'] = args.val
     
-    with open('exp_dict.json', 'w', encoding='utf-8') as f:
-        json.dump(exp_dict, f, ensure_ascii=False, indent=4)
+    # with open('exp_dict.json', 'w', encoding='utf-8') as f:
+    #     json.dump(exp_dict, f, ensure_ascii=False, indent=4)
+        
+    train_val(args , args.savedir_base)
